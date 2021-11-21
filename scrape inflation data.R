6feb0e1d7f8c9697e799944fac2612420f88a093
Patrick Callahan <patricktcallahan18@gmail.com>
2021-11-21 17:17:34 -0600

    Update scraping to use a different data source that had data to 2 decimal places. Retain only the old website for the latest year of data if it is needed

--- scrape inflation data.R
+++ scrape inflation data.R
@@ -4,60 +4,48 @@ library(rvest)
 library(tidyverse)
 library(lubridate)
 
-usa_inflation_url <- "https://www.usinflationcalculator.com/inflation/historical-inflation-rates/"
 
-usa_inflation_page <- read_html(usa_inflation_url)
 
-usa_inflation_table_scraped <- usa_inflation_page %>%
+# Scrape main data! This site was chosen because it has inflation info to
+# the second decimal place. Downside is that it doesn't not have monthly
+# data for the current year
+calculator_net_inflation_url <- "https://www.calculator.net/inflation-calculator.html"
+calculator_net_inflation_page <- read_html(calculator_net_inflation_url)
+
+calculator_net_inflation_table_scraped <- calculator_net_inflation_page %>%
+  html_element(xpath = "/html/body/div[3]/div[1]/table[4]") %>%
+  html_table(header = T) %>%
+  mutate(across(Jan:Annual, ~as.numeric(str_remove_all(.x, "%"))) )
+
+
+# Scrape auxiliary data! This site has the monthly data for the current year,
+# but it only has data to one decimal place so use of this data should be
+# kept to a minimum
+us_inflation_calculator_url <- "https://www.usinflationcalculator.com/inflation/historical-inflation-rates/"
+us_inflation_calculator_page <- read_html(us_inflation_calculator_url)
+
+us_inflation_calculator_table_scraped <- us_inflation_calculator_page %>%
   html_element(xpath = "/html/body/div[1]/div[2]/div[1]/div[1]/div/article/div/div[1]/table") %>%
   html_table() %>%
-  arrange(desc(Year)) %>%
-  mutate(year_to_year_inflation = case_when(!is.na(Ave) ~ Ave,
-                                            is.na(Ave) ~
-                                              select(.,Year,Jan:Dec) %>%
-                                              pivot_longer(-Year) %>%
-                                              group_by(Year) %>%
-                                              dplyr::filter(any(is.na(value))) %>%
-                                              ungroup() %>%
-                                              mutate(month_number = 1:12) %>%
-                                              dplyr::filter(!is.na(value)) %>% dplyr::filter(month_number == max(month_number)) %>%
-                                              pull(value)
-
-                                            )
-         )
-
-usa_inflation_table_cleaned <- usa_inflation_table_scraped %>%
-  # turn percentages into rates
-  mutate(across(Jan:year_to_year_inflation, ~.x/100)) %>%
-  # calculate number of days in a given year. will always be one of the following:
-  # A) 365 days, B) 366 days (leap years), or C) the number of days that have passed in the current year
-  mutate(elapsed_days_in_year = case_when(Year == year(Sys.Date()) ~ difftime(Sys.Date(),
-                                                                      as_date(paste(Year,"-01-01")),
-                                                                      units = "days" ),
-                                  Year != year(Sys.Date()) ~ difftime(as_date(paste(dplyr::lag(Year),"-01-01")),
-                                                                      as_date(paste(Year,"-01-01")),
-                                                                      units = "days" )) %>% as.numeric()
-         ) %>%
-  # calculate the relative daily inflation rate for each year given the yearly inflation rate
-  # and the number of days in each year
-  mutate(relative_daily_inflation_rate_of_year = (pracma::nthroot(1+year_to_year_inflation, elapsed_days_in_year) - 1))
+  dplyr::filter(Year == max(Year)) %>%
+  rename(Annual = Ave)
 
 
 
 
+# If the Calculator.net table is not yet updated to the same year as the
+# USInflationCalculator.com site, then use the data available in the latter site
+# (e.g. bind the latest year from the USInflationCalc to Calculator.net data)
 
-# For current year when annualized inflation rate is missing, need to just fill in with otherwise most recent
-# monthly value
-
-# usa_inflation_table_cleaned %>%
-#   select(Year,Jan:Dec) %>%
-#   pivot_longer(-Year) %>%
-#   group_by(Year) %>%
-#   dplyr::filter(any(is.na(value))) %>%
-#   ungroup() %>%
-#   mutate(month_number = 1:12) %>%
-#   dplyr::filter(!is.na(value)) %>% dplyr::filter(month_number == max(month_number)) %>%
-#   pull(value)
+if (max(pull(calculator_net_inflation_table_scraped,Year)) == pull(us_inflation_calculator_table_scraped,Year)) {
+  calculator_net_inflation_table_scraped <- calculator_net_inflation_table_scraped
+} else {
+  calculator_net_inflation_table_scraped %<>%
+    bind_rows(us_inflation_calculator_table_scraped) %>%
+    arrange(desc(Year))
+}
 
 
+rm(calculator_net_inflation_url,calculator_net_inflation_page,
+   us_inflation_calculator_url,us_inflation_calculator_page,us_inflation_calculator_table_scraped)
 
